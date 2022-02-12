ec8fcd87cf12a90c0434f558c1573d5ed198eba3
Albus Lee <albuslee@gmail.com>
2022-02-12 19:07:38 +1100

    finish test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,23 @@
 const { httpGet } = require('./mock-http-interface');
 
+const generateFormatedResult = async (url) => {
+  const response = await httpGet(url);
+  const { status, body } = response;
+  const message = JSON.parse(body).message;
+  if (status === 200) {
+    return { 'Arnie Quote': message };
+  } else {
+    return { 'FAILURE': message };
+  }
+}
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  let results = [];
+  await Promise.all(urls.map(async (url) => {
+    const result = await generateFormatedResult(url);
+    results.push(result);
+  }));
+  return results;
 };
 
 module.exports = {
